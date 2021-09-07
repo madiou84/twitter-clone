ee814add9406efc2543afa018af43af467cd5df0
Madiou BAH <mmadioubah@gmail.com>
2021-09-07 18:01:54 +0000

    Feat(Update node js server)

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '.', 'build');
+const port = process.env.PORT || 8000;
 
 app.use(express.static(publicPath));
 
